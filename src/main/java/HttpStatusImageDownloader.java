b47fc7d1486f532b80fe9e3e60e03e79049a7550
AOLebets <89778658+AOLebets@users.noreply.github.com>
2022-12-13 15:09:39 +0200

    Update HttpStatusImageDownloader.java

--- src/main/java/HttpStatusImageDownloader.java
+++ src/main/java/HttpStatusImageDownloader.java
@@ -10,27 +10,25 @@ public class HttpStatusImageDownloader {
             HttpStatusChecker httpStatusChecker = new HttpStatusChecker();
             String forUrl = httpStatusChecker.getStatusImage(code);
             String separator = FileSystems.getDefault().getSeparator();
-            if (!forUrl.equals(HttpURLConnection.HTTP_NOT_FOUND)) {
-                URL url = new URL(forUrl);
+            URL url = new URL(forUrl);
 
-                InputStream in = new BufferedInputStream(url.openStream());
-                ByteArrayOutputStream out = new ByteArrayOutputStream();
-                byte[] buf = new byte[1024];
-                int n;
-                while (-1!=(n=in.read(buf)))
-                {
-                    out.write(buf, 0, n);
-                }
-                out.close();
-                in.close();
-                byte[] response = out.toByteArray();
-
-                FileOutputStream fos = new FileOutputStream("." + separator + "image" + code + ".jpg");
-                fos.write(response);
-                fos.close();
-                System.out.println("Image downloaded to root folder of this program." +
-                        "\nAny time you wish to stop write \"STOP\".");
+            InputStream in = new BufferedInputStream(url.openStream());
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            byte[] buf = new byte[1024];
+            int n;
+            while (-1!=(n=in.read(buf)))
+            {
+                out.write(buf, 0, n);
             }
+            out.close();
+            in.close();
+            byte[] response = out.toByteArray();
+
+            FileOutputStream fos = new FileOutputStream("." + separator + "image" + code + ".jpg");
+            fos.write(response);
+            fos.close();
+            System.out.println("Image downloaded to root folder of this program." +
+                    "\nAny time you wish to stop write \"STOP\".");
         } catch (IOException e) {
             System.err.println(e);
         }
